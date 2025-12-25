31ebd72c8000cf4e0a6842a59535364ba27d33c1
Mitchell Merry <mitchellmerry145@gmail.com>
2025-12-25 16:07:05 +1100

    teb

--- src/game_engine/unity/mono/image.rs
+++ src/game_engine/unity/mono/image.rs
@@ -1,9 +1,10 @@
+use alloc::format;
 use core::iter::{self, FusedIterator};
 
 use super::CSTR;
 use super::{Class, Module};
 use crate::future::retry;
-use crate::{Address, Process};
+use crate::{print_message, Address, Process};
 
 /// An image is a .NET DLL that is loaded by the game. The `Assembly-CSharp`
 /// image is the main game assembly, and contains all the game logic.
@@ -57,6 +58,8 @@ impl Image {
                     .ok()
                     .filter(|val| !val.is_null());
 
+                print_message(&format!("ok, a class: {class}"));
+                panic!("aga");
                 Some(Class { class })
             })
             .fuse()
